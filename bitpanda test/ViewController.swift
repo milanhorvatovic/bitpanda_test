0825407d314ffadbe17fae1c44fdac240e4a43fb
Milan Horvatovic <imsoft.labs@gmail.com>
2019-08-05 00:36:37 +0200

    [+] single view project

---
+++ bitpanda test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitpanda_test
+//
+//  Created by Milan Horvatovic on 05/08/2019.
+//  Copyright © 2019 Milan Horvatovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
